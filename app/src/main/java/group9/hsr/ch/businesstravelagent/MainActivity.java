0b86f78e8b44fa9e8ca44b1ecd606c14d07d469f
m5sgh2a <m5sgh2a@gmail.com>
2017-01-31 19:37:19 +0100

    initial commit

--- app/src/main/java/group9/hsr/ch/businesstravelagent/MainActivity.java
+++ app/src/main/java/group9/hsr/ch/businesstravelagent/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     ConnectionList connectionList = repo.searchConnections("Buchs SG", "Zürich HB");
     */
-    //
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {

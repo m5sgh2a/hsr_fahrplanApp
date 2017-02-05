f1ad9670026f1f86b66e5ce834e8559f612143ed
m5sgh2a <m5sgh2a@gmail.com>
2017-02-05 23:16:11 +0100

    main activity

--- app/src/main/java/group9/hsr/ch/businesstravelagent/Controller/MainActivity.java
+++ app/src/main/java/group9/hsr/ch/businesstravelagent/Controller/MainActivity.java
@@ -1,8 +1,20 @@
 package group9.hsr.ch.businesstravelagent.Controller;
 
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Locale;
 
 import ch.schoeb.opendatatransport.IOpenTransportRepository;
 import ch.schoeb.opendatatransport.OpenDataTransportException;
@@ -20,10 +32,60 @@ public class MainActivity extends AppCompatActivity {
     ConnectionList connectionList = repo.searchConnections("Buchs SG", "Zürich HB");
     */
 
+    Button date;
+    Button time;
+    DatePickerDialog datePickerDialog;
+    TimePickerDialog timePickerDialog;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        date = (Button)findViewById(R.id.connectionDateButton);
+        time = (Button)findViewById(R.id.connectionTimButton);
+
+        date.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener()
+                {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        Locale locale = getResources().getConfiguration().getLocales().get(0);
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE dd.MM.yyyy", locale);
+                        date.setText(dateFormat.format(calendar));
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        time.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener()
+                {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        time.setText(hour + ":" + minute);
+                    }
+                }, hour, minute, true);
+                timePickerDialog.show();
+            }
+        });
+
     }
 }
